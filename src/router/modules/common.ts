6c54a2f1dd07acff58a283ee11f4634958bb46dc
WangHuamin <987599815@qq.com>
2023-12-29 15:36:43 +0800

    config: 配置@路径别名

--- src/router/modules/common.ts
+++ src/router/modules/common.ts
@@ -2,7 +2,7 @@ export default [
 	{
 		path: '/login',
 		name: 'loginIndex',
-		component: () => import('../../views/login/index.vue'),
+		component: () => import('@/views/login/index.vue'),
 		meta: {
 			title: '登录页'
 		}
